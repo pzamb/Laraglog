1f282da7f88faddabe996e00b8fb9ab06b177b5e
Pedro Zambrano <pzambrano02@enaltis.com>
2021-03-01 12:49:24 +0100

    Implementació de Vue3 a Laravel 8

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,30 +6,16 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue').default;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import { createApp } from 'vue' 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-    data:{
-        message:"Hello Vue!"
+createApp({
+    data(){
+        return{
+            message: "Hello Vue!",
+            posts:['Título 1','Título 2','Título 3','Título 4','Título 5'],
+            nombre:"PEDRO"
+        }
     }
-});
+}).mount('#app')
--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,4 +1,10 @@
 @extends('web.master')
 @section('content')
     @{{message}}
+
+    <div v-for="post in posts">
+        @{{post}}
+    </div>
+
+    @{{nombre}}
 @endsection
